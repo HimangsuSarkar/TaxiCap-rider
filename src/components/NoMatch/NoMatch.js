f46603467b0a8ebe1a2527210de321e41b7f625c
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-03-19 01:49:46 +0800

    use react router dom

---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div>
+            <h1>Your path is wrong.please write down exact path.</h1>
+        </div>
+    );
+};
+
+export default NoMatch;
