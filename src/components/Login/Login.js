bc2155280bb6f22fdf3890d5d1df97e5c0709a70
HimangsuSarkar <himangsusarkar1@gmail.com>
2021-03-19 18:00:36 +0800

    added google authentication

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,13 +5,28 @@ import { Form } from 'react-bootstrap';
 import { useForm } from "react-hook-form";
 import './Login.css';
 
-const Login = () => {
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config'
 
+export const initializeLOgInFramework = () => {
+    if (!firebase.apps.length) {
+        firebase.initializeApp(firebaseConfig);
+    } else {
+        firebase.app();
+    }
+
+}
+
+
+const Login = () => {
+    const [newUser, setNewUser] = useState(false);
     const [user, setUser] = useState({
         isSignedIn: false,
         name: '',
         email: '',
         password: '',
+        error: ''
     })
     const handleBlur = (e) => {
         let isFieldValid = true;
@@ -30,20 +45,70 @@ const Login = () => {
         }
 
     }
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        if (newUser && user.email && user.password) {
+            firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+                .then(res => {
+                    const newUserInfo = { ...user };
+                    newUserInfo.error = '';
+                    newUserInfo.success = true;
+                    setUser(newUserInfo);
+                })
+                .catch(error => {
+                    const newUserInfo = { ...user };
+                    newUserInfo.error = error.message;
+                    newUserInfo.success = false;
+                    setUser(newUserInfo);
+                    updateUserName(user.name);
+                });
+        }
+        if (!newUser && user.email && user.password) {
+            firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+                .then((res) => {
+                    const newUserInfo = { ...user };
+                    newUserInfo.error = '';
+                    newUserInfo.success = true;
+                    setUser(newUserInfo);
+                    console.log('sign in user info', res.user);
+                })
+                .catch((error) => {
+                    const newUserInfo = { ...user };
+                    newUserInfo.error = error.message;
+                    newUserInfo.success = false;
+                    setUser(newUserInfo);
+                });
+        }
 
+        e.preventDefault();
+    }
+    const updateUserName = name => {
+        const user = firebase.auth().currentUser;
+
+        user.updateProfile({
+            displayName: name,
+        }).then(function () {
+            console.log('user name updated successfully')
+        }).catch(function (error) {
+            console.log(error)
+        });
     }
 
     return (
 
         <div>
             <h1>Email:{user.email}</h1>
+            <input type="checkbox" name="newUser" onChange={() => setNewUser(!newUser)} />
+            <label htmlFor="newUser">New User Sign Up</label><br />
             < form onSubmit={handleSubmit} >
-                <input type="text" name="name" onBlur={handleBlur} placeholder="Enter your Name" /><br /><br />
+                {newUser && <input type="text" name="name" onBlur={handleBlur} placeholder="Enter your Name" />}<br />
                 <input type="email" name="email" onBlur={handleBlur} placeholder="Enter your Email" required /><br /><br />
                 <input type="password" name="password" onBlur={handleBlur} placeholder="Enter your Password" required /><br /><br />
                 <input type="submit" value="Sign In" />
             </form >
+            <p style={{ color: 'red' }}> {user.error}</p>
+            {
+                user.success && <p style={{ color: 'green' }}> {newUser ? 'Created' : 'Logged In'} successfully!</p>
+            }
         </div>
     );
 }
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAmeMvW68VhOZijmJTR3pk0I5j1lfixarQ",
+    authDomain: "taxicap-rider.firebaseapp.com",
+    projectId: "taxicap-rider",
+    storageBucket: "taxicap-rider.appspot.com",
+    messagingSenderId: "189666160193",
+    appId: "1:189666160193:web:8de8d30de210d3097de0c0"
+};
+
+export default firebaseConfig;
